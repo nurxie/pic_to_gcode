4a4a2187864464511cb77961fe9d25296da0160a
nurxie <pavel.usatiuk@gmail.com>
2022-12-04 03:25:50 +0200

    4

--- main.c
+++ main.c
@@ -7,10 +7,10 @@
 //34.203.60.141
 
 int main(){
-    system("python3 image_to_gcode.py --input image.png --output graph.gcode --threshold 80");
+    system("python3 image_to_gcode.py --input image.jpg --output graph.gcode --threshold 80");
     system("cp /var/www/firstimeke/maps/pic_to_gcode/graph.gcode /var/www/firstimeke/maps/");
-    system("rm image.png & rm graph.gcode");
-    sleep(10);
-    system("cd.. & rm graph.gcode");
+    system("rm image.jpg & rm graph.gcode");
+    sleep(5);
+    system("rm /var/www/firstimeke/maps/graph.gcode");
     return 0;
 }
